fb5c20ce4dbdaca438ce761d24ea7e00d04fa9e5
RomL <rom.laborde@orange.fr>
2021-08-20 17:17:02 +0200

    Ajout de la route et du controller pour ajouter un like

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -4,8 +4,11 @@ const sauceController = require('../controllers/sauce');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-router.post('/', auth, multer,sauceController.createSauce);
-router.get('/:id', auth, sauceController.getOneSauce);
 router.get('/', auth, sauceController.getAllSauces);
+router.get('/:id', auth, sauceController.getOneSauce);
+router.post('/', auth, multer,sauceController.createSauce);
+router.put('/:id', auth, multer, sauceController.modifyOneSauce);
+router.delete('/:id', auth, sauceController.deleteOneSauce);
+router.post('/:id/like', auth, sauceController.createStateLike);
 
 module.exports = router;
