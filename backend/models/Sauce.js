aa42dacdd90827d24250bf74d069069e34be0b4f
RomL <rom.laborde@orange.fr>
2021-08-25 20:53:26 +0200

    Ajout controle des champs du formulaire pour ajout et modification sauce

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,11 +1,44 @@
 const mongoose = require('mongoose');
+const sauceInfoRegex = /^[A-Z][A-Za-z\é\è\ê\ï\-]+$/;
 
 const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true },
-    name: { type: String, required: true },
-    manufacturer: { type: String, required: true },
-    description: { type: String, required: true },
-    mainPepper: { type: String, required: true },
+    name: { 
+        type: String,
+        validate: {
+            validator: function(v) {
+              return sauceInfoRegex.test(v);
+            },
+            message: props => `${props.value} n'est pas un nom valide`
+        },
+        required: true },
+    manufacturer: {
+        type: String,
+        validate: {
+            validator: function(v) {
+              return sauceInfoRegex.test(v);
+            },
+            message: props => `${props.value} n'est pas un nom de fabricant valide`
+        },
+        required: true },
+    description: {
+        type: String,
+        validate: {
+            validator: function(v) {
+              return sauceInfoRegex.test(v);
+            },
+            message: props => `Vérifier que la description ne comporte pas de caractère spéciaux hormis "," et "."`
+        },
+        required: true },
+    mainPepper: {
+        type: String,
+        validate: {
+            validator: function(v) {
+              return sauceInfoRegex.test(v);
+            },
+            message: props => `${props.value} n'est pas un nom d'ingrédient valide`
+        },
+        required: true },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true },
     likes: { type: Number },
